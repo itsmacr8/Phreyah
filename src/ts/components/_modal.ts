c6ca15d2b1764c13529554f8a0802977943e5142
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-05-04 12:36:19 +0600

    refactor: simpify modal components

--- src/ts/components/_modal.ts
+++ src/ts/components/_modal.ts
@@ -6,29 +6,27 @@ const modalContent = document.querySelector('.modal__content') as HTMLDivElement
 
 function showModal(modal:HTMLDivElement) {
     modal.classList.remove('modal--hidden')
-    modalOverlay.classList.remove('modal__overlay--hidden')
 }
 
 function closeModal(modal:HTMLDivElement) {
     modal.classList.add('modal--hidden')
-    modalOverlay.classList.add('modal__overlay--hidden')
 }
 
 // open modal window
 modalButton.addEventListener("click", function () {
-    showModal(modalContent)
+    showModal(modal)
 })
 
 // close modal window with click on close button or outside of the modal
 modal.addEventListener("click", function (e) {
     const target = e.target as HTMLElement
-    if (target.classList.contains('modal__close')) closeModal(modalContent)
-    else if (target.classList.contains('modal__overlay')) closeModal(modalContent)
+    if (target.classList.contains('modal__close')) closeModal(modal)
+    else if (target.classList.contains('modal__overlay')) closeModal(modal)
 })
 
 // close modal window with keyboard (Escape key)
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modalContent.classList.contains('hidden')) {
-        closeModal(modalContent);
+        closeModal(modal);
     }
 });
